101611206beee1f76c34097621f72c39f1a92e7c
Graham <graham.moss@hotmail.com>
2021-12-16 22:17:01 -0500

    refactor to include context api usage

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackForm({ handleAdd }) {
+function FeedbackForm() {
+  const { addFeedback } = useContext(FeedbackContext)
 
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
@@ -30,7 +32,7 @@ function FeedbackForm({ handleAdd }) {
     e.preventDefault();
     if (text.trim().length > 10) {
       const newFeedback = { text, rating }
-      handleAdd(newFeedback)
+      addFeedback(newFeedback)
       setText('')
     }
   }
