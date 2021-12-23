43bac6f510cae2db9e18f33cc61b3adbd5f1738e
Graham <graham.moss@hotmail.com>
2021-12-22 22:51:01 -0500

    Add functionality to add, update and delete content from the JSON-server backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -14,20 +13,30 @@ export const FeedbackProvider = ({ children }) => {
   }, [])
 
   const fetchFeedback = async () => {
-    const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+    const response = await fetch('/feedback?_sort=id&_order=desc')
     const data = await response.json()
 
     setFeedback(data)
     setIsLoading(false)
   }
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+
+    setFeedback([data, ...feedback])
   }
 
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Delete review?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   }
@@ -36,8 +45,18 @@ export const FeedbackProvider = ({ children }) => {
     setFeedbackEdit({ item, edit: true })
   }
 
-  const updateFeedback = (id, newItem) => {
-    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...newItem } : item));
+  const updateFeedback = async (id, newItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newItem)
+    })
+
+    const data = await response.json()
+
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item));
 
   }
 
